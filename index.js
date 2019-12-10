523ce93bdc12d00b6fd621cb5815d6b9b3109966
plbin97 <i@teenet.me>
2019-12-10 17:07:24 -0500

    new

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 let ex = require('express');
 let app = ex();
+let cors = require("cors");
 let multer = require('multer');
 let upload = multer({
     storage: multer.memoryStorage()
 });
-
+app.use(cors());
 app.post('/',upload.single("file"),require('./src/upload'));
 app.listen(12345);
